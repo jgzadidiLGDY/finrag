db760543ec8b84c8c047b2ced2a3b21de6c0b433
Jeffery Zou <jgzadidi@gmail.com>
2025-11-11 21:11:44 -0600

    remove test files from repository

--- sanity.py
+++
@@ -1,9 +0,0 @@
-# sanity.py
-from openai import OpenAI
-import tiktoken
-
-client = OpenAI()
-enc = tiktoken.encoding_for_model("text-embedding-3-small")
-print("token_count:", len(enc.encode("Hello FinRAG")))
-e = client.embeddings.create(model="text-embedding-3-small", input="Hello FinRAG")
-print("emb_dim:", len(e.data[0].embedding))
--- test.py
+++
@@ -1,9 +0,0 @@
-from llm_answer import llm_answer
-
-print(llm_answer('Explain AAPL 2024 Q2 revenue change','AAPL',2024,['Q2'])['answer'][:600])
-
-
-
-
-
-
